1fc5d6cccefa042c71a8d4746026d364e6ca4770
Himaja <Himaja@LMINHYDTST0007>
2018-05-01 20:01:57 +0530

    first commit

---
+++ Models/Categories.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var categoriesSchema = mongoose.Schema({
+	categoryID:{
+		type:Number,
+		required:true
+	},
+	categoryName:{
+		type:String,
+		required:true
+	},
+	categoryCode:{
+		type:String,
+		required:true
+	},
+	categoryDescription:{
+		type:String,
+		required:true
+    },
+    createdDate:{
+        type:Date,
+        required:true
+    }	
+});
+
+var categories = module.exports = mongoose.model('categorys',categoriesSchema);
+
+module.exports.getAllCategories = function(callback){
+    categories.find(callback);
+}
