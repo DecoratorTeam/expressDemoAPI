c66613f3e7a80c72eef80c0ab354b25a77535fb4
Himaja <Himaja@LMINHYDTST0007>
2018-05-02 10:26:52 +0530

    first commit

--- Models/Orders.js
+++ Models/Orders.js
@@ -1,3 +1,5 @@
+var mongoose = require('mongoose');
+
 module.exports.getOrderById = function(orderID,callback){        
     mongoose.connection.db.eval('GetOrdersByID('+ orderID +')',callback);
 }
